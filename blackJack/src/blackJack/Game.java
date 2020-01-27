6006131f4329371ec41ba839674ce72b760ee754
Junghyun Shim <58623389+Junghyun-Shim@users.noreply.github.com>
2020-01-28 02:50:05 +0900

    Add files via upload

--- blackJack/src/blackJack/Game.java
+++ blackJack/src/blackJack/Game.java
@@ -55,26 +55,49 @@ public class Game {
 		
 		System.out.println("The deck is shuffled");
 		dealer.shuffleDeck();
-		
+		System.out.println(dealer.deck.get(0)); // 덱 확인
+	
 		// 플레이어 수만큼 손의 패 상태 정보 만듬
-		String[][] hand = new String[game.playerNumber+1][];
+//		String[][] hand = new String[game.playerNumber+1][];
+		ArrayList<ArrayList<String>> playerHand = new ArrayList<ArrayList<String>>();
 		
-		hand[0][0] = dealer.deck.get(0);
-		System.out.println(hand[0][0]);
-		// 딜러와 플레이어들에게 카드 나눠줌
-//		for (int i=0; i<game.playerNumber+1; i++) {
-//			hand[i][0] = dealer.deck.get(0);
-//			dealer.deck.remove(0);
-//		}
-//		for (int i=0; i<=game.playerNumber; i++) {
-//			hand[i][1] = dealer.deck.get(0);
-//			dealer.deck.remove(0);
+//		
+		for (int i=0; i<game.playerNumber+1; i++) {
+			ArrayList<String> hand = new ArrayList<String>(); //hand 초기화
+			hand.add(dealer.deck.get(0)); // 덱 첫번째 카드를 핸드에 추가
+			dealer.deck.remove(0);  // 추가한 첫번째 카드를 덱에서 삭제
+			hand.add(dealer.deck.get(0)); // 다시 덱의 첫번째 카드를 손에 추가
+			dealer.deck.remove(0); // 추가한 카드를 덱에서 삭제
+			playerHand.add(hand); // 2개의 카드를 플레이어에 추가
+		}
+		
+		
+		System.out.println("< Dealer's card >");
+		System.out.println(playerHand.get(0));
+		
+		for (int i=0; i<game.playerNumber; i++) {
+		    System.out.println("< " + players.get(i) +"'s card >");
+		    System.out.println(playerHand.get(i+1));
+		}
+		
+//		String[] deck2 = {"sucess"};
+////		hand[0][0] = dealer.deck.get(0);
+//		hand[0][0] = "dfs";
+//		System.out.println(hand[0][0]);
+//		// 딜러와 플레이어들에게 카드 나눠줌
+////		for (int i=0; i<game.playerNumber+1; i++) {
+////			hand[i][0] = dealer.deck.get(0);
+////			dealer.deck.remove(0);
+////		}
+////		for (int i=0; i<=game.playerNumber; i++) {
+////			hand[i][1] = dealer.deck.get(0);
+////			dealer.deck.remove(0);
 //		}
 		
 		
 //	    // 현재 패 정보 출려
-		System.out.println("< Dealer's card >");
-		System.out.println(hand[0]);
+//		System.out.println("< Dealer's card >");
+//		System.out.println(hand[0]);
 //		
 //		for (int i=1; i<game.playerNumber+1; i++) {
 //			System.out.println("< " + players.get(i-1) +"'s card >");
