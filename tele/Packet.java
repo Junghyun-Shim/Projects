4d4bde8e67ece6e4d518a844317145010a0fe642
Junghyun Shim <58623389+Junghyun-Shim@users.noreply.github.com>
2020-02-05 12:53:22 +0900

    Add files via upload

--- tele/Packet.java
+++ tele/Packet.java
@@ -1,16 +1,25 @@
 package tele;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Packet {
     private ArrayList<Item> items = new ArrayList<Item>();
+    private HashMap<String, Item> nameAccess = new HashMap<String, Item>();
     
-    public void addItem(Item item){
-       this.items.add(item);
-    }
     
-    public Item getItem(int index) {
-    	return this.items.get(index);
+	public void addItem(Item item) {
+		this.items.add(item);
+		
+		if (nameAccess.containsKey(item.getName())) {
+			throw new RuntimeException(
+					"Duplicated item name:["+item.getName()+"]");   	
+		}
+		nameAccess.put(item.getName(), item);
+	}
+    
+    public Item getItem(String name) {
+    	return this.nameAccess.get(name);
     }
     
     public String raw()
@@ -25,7 +34,18 @@ public class Packet {
     
     public void parse(String data)
     {
-    	byte[] bdata = data.getBytes();
+    	byte[] bdata = data.getBytes(); //¿Ã∞≈ πª±Ó?
+    	int pos = 0;
+    	
+    	for (Item item: items)
+    	{
+    		byte[] temp = new byte[item.getLength()];
+    		System.arraycopy(bdata, pos, temp, 0, item.getLength());
+    		pos += item.getLength();
+    		item.setValue(new String(temp));
+    	}
+    	
+
     	
     }
 }
